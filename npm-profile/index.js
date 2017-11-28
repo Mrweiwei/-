589d54d2e0531c5ae54015137ca1888c21429533
魏巍 <827045489@qq.com>
2017-11-28 16:23:37 +0800

    Update index.js

--- npm-profile/index.js
+++ npm-profile/index.js
@@ -1,19 +1,38 @@
+//在严格模式下写代码 
 'use strict'
+//引入make-fetch-happen模块（一个node-fetch-npm包含附加功能的Node.js库，node-fetch并不打算包括HTTP缓存支持，请求池，代理，重试等等！），并设置其
+//默认属性中的请求重试设置的属性值为false（即不请求重试）。
 const fetch = require('make-fetch-happen').defaults({retry: false})
+//引入aproba模块（一个轻量级函数参数验证器）
 const validate = require('aproba')
+//引入url模块（用于URL解析和解析的实用程序）
 const url = require('url')
 
+//exports 变量是在模块的文件级别作用域内有效的，它在模块被执行前被赋予 module.exports 的值。
+//它有一个快捷方式，以便 module.exports.f = ... 可以被更简洁地写成 exports.f = ...。 
+//exports是模块往外暴露方法的接口。
+//暴露增加新用户的函数方法
 exports.adduser = adduser
+//暴露登录的函数方法
 exports.login = login
+//暴露获取的函数方法
 exports.get = get
+//暴露设置的函数方法
 exports.set = set
+//暴露列表token验证方法
 exports.listTokens = listTokens
+//暴露移除token验证方法
 exports.removeToken = removeToken
+//暴露创建token验证方法
 exports.createToken = createToken
 
+//增加新用户的的函数adduser，传入四个参数（用户名，邮箱，密码，配置）
 function adduser (username, email, password, conf) {
+  //验证传入的四个参数，用户名必须是字符串类型的，邮箱是字符串类型的，密码是字符串类型的，配置是对象类型的。
   validate('SSSO', arguments)
+  //如果不是配置的用户不是
   if (!conf.opts) conf.opts = {}
+  //设置常对象用户对象，里面的id属性是
   const userobj = {
     _id: 'org.couchdb.user:' + username,
     name: username,
@@ -33,6 +52,8 @@ function adduser (username, email, password, conf) {
 
   return fetchJSON({target: target, method: 'PUT', body: userobj, opts: conf.opts})
 }
+
+
 
 function login (username, password, conf) {
   validate('SSO', arguments)
